169151a1bd7e53eb485065ce0ade0477c1d0aa64
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-04-09 18:47:52 +0000

    Deploy iodide-project/pyodide-demo to github.com/iodide-project/pyodide-demo.git:master

---
+++ pyodide_dev.js
@@ -0,0 +1,22 @@
+var pyodide = {}
+
+{
+    let baseURL = "";
+    let wasmURL = baseURL + 'pyodide.asm.wasm';
+    let wasmXHR = new XMLHttpRequest();
+    wasmXHR.open('GET', wasmURL, true);
+    wasmXHR.responseType = 'arraybuffer';
+    wasmXHR.onload = function() {
+        if (wasmXHR.status === 200 || wasmXHR.status === 0) {
+            pyodide.wasmBinary = wasmXHR.response;
+        } else {
+            alert("Couldn't download the pyodide.asm.wasm binary.  Response was " + wasmXHR.status);
+        }
+
+        pyodide.baseURL = baseURL;
+        var script = document.createElement('script');
+        script.src = baseURL + "pyodide.asm.js";
+        document.body.appendChild(script);
+    };
+    wasmXHR.send(null);
+}
